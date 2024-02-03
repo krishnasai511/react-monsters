89ef1435e4dbe7bfa71bfe6664d271baca535720
Sai Krishna Yadav Kotte <saikrishnayadavkotte@Sais-MBP-2.lan>
2024-02-03 12:19:26 -0600

    final touch

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox className={'monsters-search-box'} onChangeHandler={onSearchChange}/>
         <CardList monsters={filteredMosters}/>
       </div>
